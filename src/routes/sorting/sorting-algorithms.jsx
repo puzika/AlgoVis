d1fab589798287d5d861b682029bc132b297decf
puzika <tolibjonlatipovk@gmail.com>
2024-08-05 16:19:34 +0700

    implemented heapsort and mergesort algorithms (no visualization)

--- src/routes/sorting/sorting-algorithms.jsx
+++ src/routes/sorting/sorting-algorithms.jsx
@@ -1,3 +1,5 @@
+import { algNames } from "./sorting-algorithm-names";
+
 export const maxVal = 50;
 export const minVal = 5;
 
@@ -11,4 +13,74 @@ export function generateRandomArray(size) {
    for (let i = 0; i < size; i++) array.push(generateRandomNumber());
 
    return array;
+}
+
+function delay(time) {
+   return new Promise(resolve => {
+      setTimeout(resolve, 1000 * 1 / time);
+   });
+}
+
+function heapify(array, size, index) {
+   let largest = index;
+   const left = 2 * index + 1;
+   const right = 2 * index + 2;
+
+   if (left < size && array[left] > array[largest]) largest = left;
+   if (right < size && array[right] > array[largest]) largest = right;
+
+   if (largest !== index) {
+      [array[index], array[largest]] = [array[largest], array[index]];
+      heapify(array, size, largest);
+   }
+}
+
+function heapSort(array) {
+   for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) heapify(array, array.length, i);
+   
+   for (let i = array.length - 1; i > 0; i--) {
+      [array[i], array[0]] = [array[0], array[i]];
+      heapify(array, i, 0);
+   }
+
+   return array;
+}
+
+function merge(leftChunk, rightChunk) {
+   const merged = [];
+   let leftIdx = 0;
+   let rightIdx = 0;
+
+   while (leftIdx < leftChunk.length && rightIdx < rightChunk.length) {
+      if (leftChunk[leftIdx] < rightChunk[rightIdx]) {
+         merged.push(leftChunk[leftIdx]);
+         leftIdx++;
+      } else {
+         merged.push(rightChunk[rightIdx]);
+         rightIdx++;
+      }
+   }
+
+   return merged.
+      concat(leftChunk.slice(leftIdx)).
+      concat(rightChunk.slice(rightIdx));
+}
+
+function mergeSort(array) {
+   if (array.length <= 1) return array;
+
+   const leftChunk = array.slice(0, Math.floor(array.length / 2));
+   const rightChunk = array.slice(Math.floor(array.length / 2));
+
+   return merge(mergeSort(leftChunk), mergeSort(rightChunk));
+}
+
+function quickSort(array, startIdx, endIdx) {
+
+}
+
+export const algorithms = {
+   'heapsort': heapSort,
+   'mergesort': mergeSort,
+   'quicksort': quickSort,
 }
--- src/routes/sorting/sorting.component.jsx
+++ src/routes/sorting/sorting.component.jsx
@@ -9,8 +9,12 @@ import AppContainer from '../../components/app-container/app-container.component
 import SortDataBar from '../../components/sort-data-bar/sort-data-bar.component';
 import SortArrayContainer from '../../components/sort-array-container/sort-array-container.component';
 import { algNames } from './sorting-algorithm-names';
-import { generateRandomArray } from './sorting-algorithms';
+import { generateRandomArray, algorithms } from './sorting-algorithms';
 import * as S from './sorting.styles';
+
+function formatAlgName(algName) {
+   return algName.replace(/\s+/g, '').toLowerCase(); //REMOVE SPACES AND CONVERT ALL UPPERCASE LETTERS TO LOWECASE ("Heap Sort => heapsort")
+}
 
 export default function Sorting() {
    const {size, setSize, algorithm, setAlgorithm} = useContext(ArrayContext);
@@ -27,6 +31,12 @@ export default function Sorting() {
       setArray(newArray);
    }
 
+   function handleSort() {
+      const algName = formatAlgName(algorithm);
+      const sortedArray = algorithms[algName]([...array]);
+      console.log(array, sortedArray);
+   }
+
    return (
       <>
          <Toolbar>
@@ -39,7 +49,7 @@ export default function Sorting() {
             </Dropdown>
             <RangeSlider name={'Size'} width={'15%'} min='10' max='200' value={size} changeHandler={handleChange} />
             <Button clickHandler={handleGenerate} name={'Generate array'} styleType={ButtonTypes.filled} />
-            <Button name={'Sort'} styleType={ButtonTypes.transparent} />
+            <Button clickHandler={handleSort} name={'Sort'} styleType={ButtonTypes.transparent} />
          </Toolbar>
          <AppContainer>
             <SortDataBar algorithm={algorithm} size={size} />
