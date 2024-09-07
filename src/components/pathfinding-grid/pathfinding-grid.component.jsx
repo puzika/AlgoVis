71de90e5d0b7a3274e31037c7e136b8b9d0129dc
puzika <tolibjonlatipovk@gmail.com>
2024-09-07 13:28:44 +0700

    fixed drag and drop and drawing walls

--- src/components/pathfinding-grid/pathfinding-grid.component.jsx
+++ src/components/pathfinding-grid/pathfinding-grid.component.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react';
 import { gridWidth } from '../../routes/pathfinding/pathfinding-algorithms';
 import sourceImg from '../../assets/source.svg';
 import destinationImg from '../../assets/destination.svg';
@@ -10,128 +9,139 @@ export default function Grid({grid, refs, start, dest, updateGrid, updateCoords}
    const [rows, cols] = [grid.length, grid[0].length];
    const [startY, startX] = start;
    const [endY, endX] = dest;
-   const gridRef = useRef(null);
+   const gridCopy = cloneDeep(grid);
 
-   useEffect(() => {
-      const gridElem = gridRef.current;
-      let drawing = false;
-      let cleaning = false;
-      let draggedElement = null;
+   let drawing = false;
+   let cleaning = false;
+   let draggedElem = null;
 
-      //DRAWING AND ERASING WALLS
-      
-      const onMouseDown = (e) => {
-         const {target} = e;
+   function isSource(y, x) {
+      return y === startY && x === startX;
+   }
 
-         if (!target.classList.contains('cell')) return;    //IF THE CLICKED ELEMENT IS NOT A CELL RETURN
+   function isTarget(y, x) {
+      return y === endY && x === endX;
+   }
 
-         e.preventDefault();     //PREVENT DRAGGING EMPTY CELL
+   //DRAWING AND ERASING WALLS
 
-         const idx = +target.dataset.idx;
-         const [y, x] = [Math.floor(idx / cols), idx % cols];     //GET THE COORDINATES OF THE CELL FROM ITS INDEX
+   function handleMouseDown(e) {    //START DRAWING / ERASING WALLS
+      const {target} = e;
 
-         if (grid[y][x] === '#') {
-            cleaning = true;
-            grid[y][x] = '';
-            removeWall(target);
-         } else {
-            drawing = true;
-            grid[y][x] = '#';
-            addWall(target);
+      if (!target.classList.contains('cell')) return;    //IF CLICKED ELEMENT NOT CELL RETURN
+
+      e.preventDefault();     //PREVENT DRAGGING CELL
+
+      const idx = +target.dataset.idx;    //GET THE INDEX OF THE CELL
+      const [y, x] = [Math.floor(idx / cols), idx % cols];     //GET THE COORDINATES OF THE CELL
+
+      if (gridCopy[y][x] === '') {     //IF THE CELL IS EMPTY START DRAWING WALLS
+         drawing = true;
+         gridCopy[y][x] = '#';
+         addWall(target);
+      } else {    //IF THE CELL IS NOT EMPTY START ERASING WALLS
+         cleaning = true;
+         gridCopy[y][x] = '';
+         removeWall(target);
+      }
+   }
+
+   function handleMouseOver(e) {    //KEEP DRAWING / ERASING WALLS 
+      const {target} = e;
+
+      if (!target.classList.contains('cell')) return;    //DO NOTHING IF THE ELEMENT THE MOUSE IS OVER IS NOT A CELL
+
+      const idx = +target.dataset.idx;    //GET THE INDEX OF THE CELL
+      const [y, x] = [Math.floor(idx / cols), idx % cols];     //GET THE COORDINATES OF THE CELL
+
+      if (drawing) {    //IF DRAWING IS TRUE DRAW A WALL
+         gridCopy[y][x] = '#';
+         addWall(target);
+      } else if (cleaning) {     //IF CLEANING IS TRUE ERASE A WALL
+         gridCopy[y][x] = '';
+         removeWall(target);
+      }
+   }
+
+   function handleMouseUp() {    //WHEN THE MOUSE UP EVENT IS TRIGGERED STOP DRAWING / CLEANING AND UPDATE GRID
+      drawing = false;
+      cleaning = false;
+      updateGrid(gridCopy);
+   } 
+
+   function handleMouseLeave() {    //WHEN TEH MOUSE LEAVES THE GRID STOP DRAWING / CLEANING AND UPDATE GRID
+      handleMouseUp();
+   }
+
+   //DRAG AND DROP
+
+   function handleDragStart(e) {    //START DRAGGING
+      const {target} = e;
+
+      draggedElem = target;   //SAVE THE REF TO THE DRAGGED ELEMENT
+   }
+
+   function handleDragOver(e) {
+      e.preventDefault();     //ALLOW DROP
+   }
+
+   function handleDrop(e) {
+      const {target} = e;
+
+      if (!target.classList.contains('cell')) return;  //IF THE DROPZONE IS NOT AN EMPTY CELL RETURN
+
+      const idx = +target.dataset.idx;    //GET THE INDEX OF THE DROP CELL
+      const [y, x] = [Math.floor(idx / cols), idx % cols];    //GET ITS COORDINATES
+
+      if (isSource(y, x) || isTarget(y, x)) return;     //IF THE CELL CONTAINS THE SOURCE NODE OR THE TARGET NODE RETURN
+
+      if (gridCopy[y][x] === '#') {    //IF THE CELL THE DRAGGED ITEM IS DROPPED ON IS A WALL REMOVE IT
+         gridCopy[y][x] = '';
+         removeWall(target);
+         updateGrid(gridCopy);
+      }
+
+      if (draggedElem.classList.contains('source')) {      //IF THE DRAGGED ITEM IS THE SOURCE NODE UPDATE SOURCE NODE COORDINATES
+         const newCoords = {
+            source: [y, x],
+            dest: [endY, endX]
          }
-      }
 
-      const onMouseOver = (e) => {
-         const {target} = e;
-
-         if (!target.classList.contains('cell')) return;
-
-         const idx = +target.dataset.idx;
-         const [y, x] = [Math.floor(idx / cols), idx % cols];
-
-         if (
-            (y === startY && x === startX) ||
-            (y === endY && x === endX)
-         ) return;
-
-         if (drawing) {
-            grid[y][x] = '#';
-            addWall(target);
-         } else if (cleaning) {
-            grid[y][x] = '';
-            removeWall(target);
-         }
-      }
-
-      const onMouseUp = (e) => {
-         drawing = false;
-         cleaning = false;
-
-         const updatedGrid = cloneDeep(grid);
-         updateGrid(updatedGrid);
-      }
-
-      //DRAG AND DROP
-
-      const onDragStart = (e) => {
-         const {target} = e;
-
-         if (target.classList.contains('source') || target.classList.contains('target')) draggedElement = target;
-      }
-
-      const onDragOver = (e) => {
-         e.preventDefault();
-      }
-
-      const onDrop = (e) => {
-         const {target} = e;
-
-         if (!target.classList.contains('cell')) return;
-
-         const idx = +target.dataset.idx;
-         const [y, x] = [Math.floor(idx / cols), idx % cols];
-
-         if (draggedElement.classList.contains('source')) {
-            updateCoords(currCoords => ({...currCoords, source: [y, x]}));
-         } else {
-            updateCoords(currCoords => ({...currCoords, dest: [y, x]}));
+         updateCoords(newCoords);
+      } else {    ////IF THE DRAGGED ITEM IS THE TARGET NODE UPDATE TARGET NODE COORDINATES
+         const newCoords = {
+            source: [startY, startX],
+            dest: [y, x]
          }
 
-         if (grid[y][x] === '#') {
-            grid[y][x] = '';
-            removeWall(target);
-            updateGrid(cloneDeep(grid));
-         }
+         updateCoords(newCoords);
       }
-
-      //DRAWING AND ERASING EVENT LISTENERS
-
-      gridElem.addEventListener('mousedown', onMouseDown);
-      gridElem.addEventListener('mouseover', onMouseOver);
-      gridElem.addEventListener('mouseup', onMouseUp); 
-      
-      //DRAG AND DROP EVENT LISTENERS
-
-      gridElem.addEventListener('dragstart', onDragStart);
-      gridElem.addEventListener('dragover', onDragOver);
-      gridElem.addEventListener('drop', onDrop);
-
-      return () => {
-         gridElem.removeEventListener('mousedown', onMouseDown);
-         gridElem.removeEventListener('mouseover', onMouseOver);
-         gridElem.removeEventListener('mouseup', onMouseUp);
-      }
-   }, []);
+   }
 
    return (
-      <S.Grid ref={gridRef} $width={gridWidth} >
+      <S.Grid 
+         $width={gridWidth} 
+         onMouseDown={handleMouseDown}
+         onMouseOver={handleMouseOver}
+         onMouseUp={handleMouseUp}
+         onMouseLeave={handleMouseLeave}
+
+         onDragOver={handleDragOver}
+         onDrop={handleDrop}
+      >
          {
             grid.map((row, i) => (
                row.map((_, j) => (
-                  <S.GridCell className='cell' ref={element => refs.current[j + i * cols] = element} key={j + i * cols} data-idx={j + i * cols}>
+                  <S.GridCell 
+                     className='cell' 
+                     ref={element => refs.current[j + i * cols] = element} 
+                     key={j + i * cols} data-idx={j + i * cols}
+                  >
                      {
-                        i === startY && j === startX ? <S.GridCellImg className='source' src={sourceImg} alt='start node' /> :
-                        i === endY && j === endX && <S.GridCellImg className='target' src={destinationImg} alt='end node' />
+                        i === startY && j === startX ? 
+                        <S.GridCellImg className='source' onDragStart={handleDragStart} src={sourceImg} alt='start node' /> :
+                        i === endY && j === endX && 
+                        <S.GridCellImg className='target' onDragStart={handleDragStart} src={destinationImg} alt='end node' />
                      }
                   </S.GridCell>
                )) 
