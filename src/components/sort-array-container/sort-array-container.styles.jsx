79ca9949d013d9bdf9db9d148acb5ae419e0dec9
puzika <tolibjonlatipovk@gmail.com>
2024-09-14 17:40:03 +0700

    made array container responsive

--- src/components/sort-array-container/sort-array-container.styles.jsx
+++ src/components/sort-array-container/sort-array-container.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { responsive } from "../../breakpoints";
 
 export const SortArrayContainer = styled.div`
    width: 100%;
@@ -6,4 +7,8 @@ export const SortArrayContainer = styled.div`
    align-items: flex-end;
    gap: .2rem;
    height: 50vh;
+
+   ${responsive.dt`
+      gap: .1rem;
+   `};
 `;
--- src/routes/sorting/sorting.component.jsx
+++ src/routes/sorting/sorting.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useRef } from 'react';
+import { useContext, useState, useRef, useEffect } from 'react';
 import { SortingContext } from '../../contexts/sorting';
 import Toolbar from '../../components/toolbar/toolbar.component';
 import Button, { ButtonTypes } from '../../components/button/button.component';
@@ -9,7 +9,14 @@ import DataBar from '../../components/data-bar/data-bar.component';
 import SortArrayContainer from '../../components/sort-array-container/sort-array-container.component';
 import { algNamesSort } from './sorting-algorithm-names';
 import { generateRandomArray, sortAlgorithms } from './sorting-algorithms';
+import { device } from '../../breakpoints';
 import * as S from './sorting.styles';
+
+const maxArraySizes = {
+   mb: 100,    //MAX ARRAY SIZE FOR MOBILE
+   lp: 150,    //MAX ARRAY SIZE FOR LAPTOP
+   dt: 200,    //MAX ARRAY SIZE FOR DESKTOP
+}
 
 function formatAlgName(algName) {
    return algName.replace(/[^A-Z0-9]+/ig, '').toLowerCase(); //REMOVE SPECIAL CHARACTERS AND CONVERT ALL UPPERCASE LETTERS TO LOWECASE ("Heap Sort => heapsort")
@@ -20,6 +27,30 @@ export default function Sorting() {
    const [array, setArray] = useState(generateRandomArray(size));
    const [maxSize, setMaxSize] = useState(200);
    const barRefs = useRef([]);
+
+   useEffect(() => {
+      const setSizeForCurrDevice = (newSize) => {
+         const defaultSize = 50;
+
+         setMaxSize(newSize);
+         setSize(defaultSize);
+         setArray(generateRandomArray(defaultSize));
+      }
+
+      const updateMaxSize = () => {
+         const currWidth = window.innerWidth;
+
+         if (currWidth <= device.mb && maxSize !== maxArraySizes.mb) setSizeForCurrDevice(maxArraySizes.mb);   //SET MAX ARRAY SIZE FOR MOBILE
+         else if (currWidth > device.mb && currWidth <= device.lp && maxSize !== maxArraySizes.lp) setSizeForCurrDevice(maxArraySizes.lp);   //BIGGER THAN MOBILE SMALLER THAN LAPTOP
+         else if (currWidth > device.lp && maxSize !== maxArraySizes.dt) setSizeForCurrDevice(maxArraySizes.dt);  //BIGGER THAN LAPTOP
+      }
+
+      updateMaxSize();
+
+      window.addEventListener('resize', updateMaxSize);
+
+      return () => window.removeEventListener('resize', updateMaxSize);
+   }, [maxSize]);
 
    function handleChange(e) {
       const newSize = e.target.value;
